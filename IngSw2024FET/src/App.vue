f44e61e31d6eb253c30996aed8247b9674c1ebd3
annaferri02 <anna.ferri@edu.unife.it>
2024-07-24 16:00:51 +0200

    modifiche

--- IngSw2024FET/src/App.vue
+++ IngSw2024FET/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/Pagamento.vue'
+import HelloWorld from './components/Biglietti.vue'
 </script>
 
 <template>
--- IngSw2024FET/src/components/Biglietti.vue
+++ IngSw2024FET/src/components/Biglietti.vue
@@ -12,29 +12,85 @@ axios.get("/api/callREST").then(response => {
   informazioni_biglietti.value = response.data;
 })
 
-
-class Biglietto {
-  "nome_concerto": string; //aggiunto
-  "dataa": string;
-  "ora": string;
-  "luogo": string;
-  "quantita_1": number;
-  "tipo_biglietto_1": string;
-  "prezzo_tipo1": number;
-  "descrizione_1": string;
-  "quantita_2": number;
-  "tipo_biglietto_2": string;
-  "prezzo_tipo2": number;
-  "descrizione_2": string;
-  "totale": number;
-}
-
+    new Vue({
+      el: '#app',
+      data: {
+        loggedUser: null,
+        applicationMessage: null,
+        menuActiveLink: 'Home',
+        biglietti: [],
+        evento: {},
+        tipo: 'Indoor',
+        numeroPosti: 0,
+        selectedSeats: []
+      },
+      computed: {
+        bigliettiString() {
+          return this.biglietti.join(',');
+        },
+        categoryOptions() {
+          if (this.tipo === 'Indoor') {
+            return [
+              { text: 'Parterre', value: 1 },
+              { text: 'Parterre VIP', value: 2 }
+            ];
+          } else if (this.tipo === 'Outdoor') {
+            return [
+              { text: 'Pit', value: 1 },
+              { text: 'Pit GOLD', value: 2 }
+            ];
+          }
+          return [];
+        },
+        allSelectedSeats() {
+          return this.selectedSeats.join(',');
+        }
+      },
+      methods: {
+        logout() {
+          // Logica per il logout
+        },
+        incrementPosti() {
+          if (this.numeroPosti < 6) {
+            this.numeroPosti++;
+          }
+        },
+        decrementPosti() {
+          if (this.numeroPosti > 0) {
+            this.numeroPosti--;
+          }
+        },
+        updateSelectedSeats(seatId) {
+          const index = this.selectedSeats.indexOf(seatId);
+          if (index === -1) {
+            this.selectedSeats.push(seatId);
+          } else {
+            this.selectedSeats.splice(index, 1);
+          }
+        },
+        proceedToPurchase() {
+          const totalSeats = this.numeroPosti + this.selectedSeats.length;
+          if (totalSeats === 0) {
+            alert('Devi selezionare almeno un posto o indicare il numero di posti desiderati.');
+            return;
+          }
+          if (totalSeats > 6) {
+            alert('Puoi selezionare al massimo 6 posti.');
+            return;
+          }
+          // Logica per procedere all'acquisto
+        }
+      },
+      mounted() {
+        // Logica per popolare la mappa dei posti basata su tipo di luogo
+      }
+    });
 </script>
+
 
 <template>
   <head>
     <title>Pagina Biglietto</title>
-    <link rel="stylesheet" href="/Users/annaferri/Desktop/ALTF6/IngSw2024FET/src/assets/main.css">
   </head>
   <body>
   <header>
@@ -52,33 +108,83 @@ class Biglietto {
     </div>
   </header>
   <main>
-    <div class="riepilogo">
-      <h1>{{ informazioni_biglietti.nome_concerto }} <span style="float: right; padding-right: 20px;"> </span></h1>
-      <h2>Biglietti disponibili</h2>
-      <div class="selezioe_biglietti">
-        <p>{{ informazioni_biglietti.tipo_biglietto_1 }} <span style="float: right; padding-right: 20px;"> {{ informazioni_biglietti.prezzo_tipo1}}</span></p>
-        <p>{{ informazioni_biglietti.descrizione_1 }}</p>
-        <br>
-        <p>{{ informazioni_biglietti.tipo_biglietto_2 }} <span style="float: right; padding-right: 20px;"> {{ informazioni_biglietti.prezzo_tipo2}}</span></p>
-        <p>{{ informazioni_biglietti.descrizione_2 }}</p>
-      </div>
-      <h2>Riepilogo</h2>
-      <p>{{informazioni_biglietti.quantita_1}} {{informazioni_biglietti.tipo_biglietto_1}} <span style="float: right; padding-right: 20px;"> {{informazioni_biglietti.prezzo_tipo1}}</span></p>
-      <p>{{informazioni_biglietti.quantita_2}} {{informazioni_biglietti.tipo_biglietto_2}} <span style="float: right; padding-right: 20px;"> {{informazioni_biglietti.prezzo_tipo2}}</span></p>
-      <!-- quando spingo il bottone mi deve portare alla pagina di pagamento -->
-      <button @click="Pagamento.vue">VAI AL PAGAMENTO</button>
-    </div>
-    <div class="immagine_concerto">
-      <div class="dati_concerto">
-        <h1>{{ informazioni_biglietti.dataa }} ore {{ informazioni_biglietti.ora }} <br> {{ informazioni_biglietti.luogo }}</h1>
-      </div>
-    </div>
+    <h1 class="centrato">Seleziona il biglietto che preferisci!</h1>
+    <br><br>
+    <p class="centrato" v-if="Luogo.tipoluogo === 'Indoor'">Seleziona i posti numerati che desideri acquistare direttamente dalla piantina e i posti in parterre dal menù sottostante</p>
+    <p class="centrato" v-if="Luogo.tipoluogo === 'Outdoor'">Seleziona i posti in Pit e Pit Gold mostrati nella piantina, dal menù sottostante</p>
+    <section v-if="Luogo.tipoluogo === 'Indoor'" class="layout-esterno">
+      <article class="tariffe">
+        <h3>Tariffe:</h3>
+        <h4>Parterre 50 &euro;</h4>
+        <h4>Parterre VIP 100 &euro;</h4>
+        <h4>Tribuna frontale 70 &euro;</h4>
+        <h4>Tribuna laterale destra 90 &euro;</h4>
+        <h4>Tribuna laterale sinistra 90 &euro;</h4>
+      </article>
+      <section class="container">
+        <table id="palco">
+          <tr>
+            <td style="text-align: center; font-size: 40px">PALCO</td>
+          </tr>
+        </table>
+        <table id="parterre">
+          <tr>
+            <td style="text-align: center; font-size: 40px">PARTERRE</td>
+          </tr>
+        </table>
+        <table id="parterreVIP">
+          <tr>
+            <td style="text-align: center; font-size: 40px">PARTERRE VIP</td>
+          </tr>
+        </table>
+        <table id="tribuna"></table>
+        <table id="tribuna-sx"></table>
+        <table id="tribuna-dx"></table>
+      </section>
+    </section>
+    <section v-if="tipo === 'Outdoor'" class="layout-esterno1">
+      <article class="tariffe">
+        <h3>Tariffe:</h3>
+        <h4>Pit 50 &euro;</h4>
+        <h4>Pit Gold 100 &euro;</h4>
+      </article>
+      <section class="container1">
+        <table id="palco1">
+          <tr>
+            <td style="text-align: center; font-size: 40px">PALCO</td>
+          </tr>
+        </table>
+        <table id="pit">
+          <tr>
+            <td style="text-align: center; font-size: 40px">PIT</td>
+          </tr>
+        </table>
+        <table id="pitgold">
+          <tr>
+            <td style="text-align: center; font-size: 40px">PIT GOLD</td>
+          </tr>
+        </table>
+      </section>
+    </section>
+    <br>
+    <h3 style="margin-top: 10%; margin-left: 26%">Seleziona la categoria di posti che desideri acquistare:</h3>
+    <select id="categoria" v-model="categoria" style="width: fit-content; margin-left: 26%">
+      <option v-for="option in categoryOptions" :value="option.value">{{ option.text }}</option>
+    </select>
+    <br>
+    <h3 style="margin-left: 26%">Seleziona il numero di posti che desideri acquistare:</h3>
+    <label style="margin-left: 26%" for="numeroPosti">Numero di posti </label>
+    <button class="bottone-scelta" @click="decrementPosti" style="border: none; font-size: 20px; font-weight: bolder">-</button>
+    <input type="number" id="numeroPosti" v-model="numeroPosti" style="margin-bottom:30px; font-size: 20px; border: none" min="0" max="6" readonly>
+    <button class="bottone-scelta" @click="incrementPosti" style="border: none; font-size: 20px; font-weight: bolder">+</button>
+    <input type="hidden" name="controllerAction" value="PagamentoManagement.gotoPagamentoBiglietto">
+    <input type="hidden" id="allSelectedSeats" :value="allSelectedSeats">
+    <input type="hidden" name="idEvento" :value="evento.idEvento">
+    <button @click="proceedToPurchase" style="margin-top: 20px">Procedi con l'acquisto</button>
   </main>
+
   <footer>
     <p>ALT + F6 - altf6@events.com - +39 3840957702 - www.events.it</p>
   </footer>
   </body>
 </template>
-
-<style src = "/Users/annaferri/Desktop/ALTF6/IngSw2024FET/src/assets/base.css"></style>
-<style src = "/Users/annaferri/Desktop/ALTF6/IngSw2024FET/src/assets/main.css"></style>
